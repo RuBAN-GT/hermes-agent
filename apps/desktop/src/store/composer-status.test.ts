Carrie111998/Hermes

fix(desktop): stop stale session RPC retries

apps/desktop/src/store/composer-status.test.ts
@@ -7,9 +7,11 @@ import {
   isSessionGoneForBackgroundPolling,
   reconcileBackgroundProcesses,
   refreshBackgroundProcesses,
-  resetBackgroundPollingGuard
+  resetBackgroundPollingGuard,
+  stopBackgroundProcess
 } from './composer-status'
 import { $gateway } from './gateway'
+import { markSessionRpcBlocked } from './session-rpc-guard'
 
 const SID = 'sess-1'
 
@@ -262,6 +264,41 @@ describe('refreshBackgroundProcesses dead-session guard', () => {
     await refreshBackgroundProcesses(SID)
 
     expect(request).toHaveBeenCalledTimes(2)
+  })
+
+  it('dismisses a stale process row when Stop is clicked after the runtime is gone', async () => {
+    reconcileBackgroundProcesses(SID, [running('stale')])
+    markSessionRpcBlocked(SID)
+    $gateway.set({ request: vi.fn() } as never)
+
+    await stopBackgroundProcess(SID, 'stale')
+
+    expect(items()).toEqual([])
+  })
+
+  it('dismisses a stale process row while the gateway is disconnected', async () => {
+    reconcileBackgroundProcesses(SID, [running('disconnected')])
+    markSessionRpcBlocked(SID)
+    $gateway.set(null as never)
+
+    await stopBackgroundProcess(SID, 'disconnected')
+
+    expect(items()).toEqual([])
+  })
+
+  it('dismisses and latches when Stop discovers the runtime is gone', async () => {
+    const request = vi.fn(async () => {
+      throw new Error('session not found')
+    })
+
+    reconcileBackgroundProcesses(SID, [running('rejected')])
+    $gateway.set({ request } as never)
+
+    await stopBackgroundProcess(SID, 'rejected')
+    await stopBackgroundProcess(SID, 'rejected')
+
+    expect(items()).toEqual([])
+    expect(request).toHaveBeenCalledTimes(1)
   })
 })
 

apps/desktop/src/store/composer-status.ts
@@ -1,4 +1,3 @@
-import { JsonRpcGatewayError } from '@hermes/shared'
 import { atom, computed } from 'nanostores'
 
 import { translateNow } from '@/i18n'
@@ -10,7 +9,13 @@ import { $goalsBySession, type GoalStatus } from './goals'
 import { dispatchNativeNotification } from './native-notifications'
 import { notifyError } from './notifications'
 import { $sessions, lineageAliases } from './session'
-import { $sessionStates } from './session-states'
+import {
+  isSessionNotFoundError,
+  isSessionRpcBlocked,
+  markSessionRpcBlocked,
+  resetSessionRpcGuard
+} from './session-rpc-guard'
+import { $sessionStates, requestForOwnedSession } from './session-states'
 import { $subagentsBySession, type SubagentProgress } from './subagents'
 import { $todosBySession } from './todos'
 
@@ -378,67 +383,39 @@ export function reconcileBackgroundProcesses(sid: string, procs: GatewayProcessE
   writeBackground(sid, next)
 }
 
-/** Session ids the gateway has told us are gone. A session-scoped RPC against a
- *  runtime the gateway no longer holds fails 4001 "session not found" — a
- *  TERMINAL condition, not the transient socket loss the catch below assumes.
- *
- *  The status stack re-polls `process.list` every 5s while a running row is on
- *  screen, so treating 4001 as transient meant re-sending the same dead id
- *  forever: one runtime id accumulated 18,614 gateway rejections in a single day
- *  (#94219 fallout). Latch the id here and skip it until something rebinds it. */
-const goneSessions = new Set<string>()
-
-/** Gateway JSON-RPC code for "session not found" (tui_gateway _sess_nowait). */
-const GATEWAY_SESSION_NOT_FOUND_CODE = 4001
-
-/** A gone session is unrecoverable for THIS runtime id; a timeout or transport
- *  blip is not. Only the former may stop the poll — misclassifying a transient
- *  failure would silently freeze the status stack on a healthy session.
- *
- *  Match the gateway's 4001 code when the error carries one (JsonRpcGatewayError
- *  from a structured RPC rejection) — a message substring alone could latch on
- *  an unrelated error class that merely mentions "session not found" (e.g. a
- *  wrapped tool/report string). The message fallback survives only for errors
- *  with no numeric code at all, where the frame's structure was lost. */
-export function isSessionGoneForBackgroundPolling(error: unknown): boolean {
-  if (error instanceof JsonRpcGatewayError && typeof error.code === 'number') {
-    return error.code === GATEWAY_SESSION_NOT_FOUND_CODE
-  }
-
-  const message = error instanceof Error ? error.message : String(error ?? '')
-
-  return /session not found/i.test(message)
-}
+/** Compatibility export for the status-stack tests and callers. */
+export const isSessionGoneForBackgroundPolling = isSessionNotFoundError
 
 /** Clear the gone-latch. Called with a session id when a fresh runtime binds to
  *  it (so polling resumes), or with no argument to reset everything (tests). */
 export function resetBackgroundPollingGuard(sid?: string): void {
-  if (sid) {
-    goneSessions.delete(sid)
-
-    return
-  }
-
-  goneSessions.clear()
+  resetSessionRpcGuard(sid)
 }
 
 /** Pull the session's live process snapshot from the gateway. */
 export async function refreshBackgroundProcesses(sid: string): Promise<void> {
   const gateway = $gateway.get()
 
-  if (!sid || !gateway || goneSessions.has(sid)) {
+  if (!sid || !gateway || isSessionRpcBlocked(sid)) {
     return
   }
 
   try {
-    const result = await gateway.request<{ processes?: GatewayProcessEntry[] }>('process.list', { session_id: sid })
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request<T>(method, params ?? {})
+    const result = await requestForOwnedSession<{ processes?: GatewayProcessEntry[] }>(
+      sid,
+      ambientRequest,
+      'process.list',
+      { session_id: sid }
+    )
 
     reconcileBackgroundProcesses(sid, result?.processes ?? [])
   } catch (error) {
     // A gone session never comes back under this runtime id: stop polling it,
     // or the 5s timer hammers the gateway with 4001s for the window's lifetime.
     if (isSessionGoneForBackgroundPolling(error)) {
-      goneSessions.add(sid)
+      markSessionRpcBlocked(sid)
 
       return
     }
@@ -468,10 +445,34 @@ export function dismissBackgroundProcess(sid: string, id: string) {
  *  row while the process lived on, stranding rogue tasks. On failure the row
  *  stays so the user can retry / see it didn't die. */
 export async function stopBackgroundProcess(sid: string, id: string): Promise<void> {
+  const gateway = $gateway.get()
+
+  if (isSessionRpcBlocked(sid)) {
+    // The backend has already declared this runtime gone, so there is no
+    // authoritative process left to kill through this session. Remove the
+    // stale local row instead of leaving the Stop button permanently inert.
+    dismissBackgroundProcess(sid, id)
+
+    return
+  }
+
+  if (!gateway) {
+    return
+  }
+
   try {
-    await $gateway.get()?.request('process.kill', { process_id: id, session_id: sid })
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request<T>(method, params ?? {})
+    await requestForOwnedSession(sid, ambientRequest, 'process.kill', { process_id: id, session_id: sid })
     dismissBackgroundProcess(sid, id)
   } catch (err) {
+    if (isSessionGoneForBackgroundPolling(err)) {
+      dismissBackgroundProcess(sid, id)
+      markSessionRpcBlocked(sid)
+
+      return
+    }
+
     notifyError(err, 'Could not stop the process')
   }
 }
@@ -498,7 +499,18 @@ export function resetSessionBackground(sid: string) {
     dismissed.add(item.id)
 
     if (item.state === 'running') {
-      void gateway?.request('process.kill', { process_id: item.id, session_id: sid }).catch(() => undefined)
+      if (gateway && !isSessionRpcBlocked(sid)) {
+        const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+          gateway.request<T>(method, params ?? {})
+        void requestForOwnedSession(sid, ambientRequest, 'process.kill', {
+          process_id: item.id,
+          session_id: sid
+        }).catch(error => {
+          if (isSessionGoneForBackgroundPolling(error)) {
+            markSessionRpcBlocked(sid)
+          }
+        })
+      }
     }
   }
 

apps/desktop/src/store/prompts.ts
@@ -2,6 +2,8 @@ import { atom, computed, type ReadableAtom } from 'nanostores'
 
 import { $clarifyRequest, $clarifyRequests } from './clarify'
 import { $activeSessionId } from './session'
+import { isSessionNotFoundError, isSessionRpcBlocked, markSessionRpcBlocked } from './session-rpc-guard'
+import { requestForOwnedSession } from './session-states'
 
 // Blocking interactive prompts the gateway raises mid-turn. Each maps to a
 // `*.request` event the Python side emits while it blocks the agent thread
@@ -119,21 +121,48 @@ export async function receiveApprovalRequest(gateway: ApprovalGateway | null, re
   setApprovalRequest(request)
 
   if (gateway && request.requestId && request.sessionId) {
-    await gateway.request('approval.received', {
-      request_id: request.requestId,
-      session_id: request.sessionId
-    })
+    try {
+      const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+        gateway.request(method, params ?? {}) as Promise<T>
+
+      await requestForOwnedSession(
+        request.sessionId,
+        ambientRequest,
+        'approval.received',
+        { request_id: request.requestId, session_id: request.sessionId }
+      )
+    } catch (error) {
+      if (isSessionNotFoundError(error)) {
+        markSessionRpcBlocked(request.sessionId)
+      }
+    }
   }
 }
 
 export async function replayPendingApproval(gateway: ApprovalGateway | null, sessionId: string | null): Promise<void> {
-  if (!gateway || !sessionId) {
+  if (!gateway || !sessionId || isSessionRpcBlocked(sessionId)) {
     return
   }
 
-  const rawResult = await gateway.request('approval.pending', {
-    session_id: sessionId
-  })
+  let rawResult: unknown
+
+  try {
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request(method, params ?? {}) as Promise<T>
+
+    rawResult = await requestForOwnedSession(
+      sessionId,
+      ambientRequest,
+      'approval.pending',
+      { session_id: sessionId }
+    )
+  } catch (error) {
+    if (isSessionNotFoundError(error)) {
+      markSessionRpcBlocked(sessionId)
+    }
+
+    return
+  }
 
   const result =
     rawResult && typeof rawResult === 'object' ? (rawResult as { approvals?: PendingApprovalPayload[] }) : {}

RuBAN-GT/hermes-agent

apps/desktop/src/store/composer-status.test.ts
@@ -7,9 +7,11 @@ import {
   isSessionGoneForBackgroundPolling,
   reconcileBackgroundProcesses,
   refreshBackgroundProcesses,
-  resetBackgroundPollingGuard
+  resetBackgroundPollingGuard,
+  stopBackgroundProcess
 } from './composer-status'
 import { $gateway } from './gateway'
+import { markSessionGone } from './runtime-gone'
 
 const SID = 'sess-1'
 
@@ -262,6 +264,41 @@ describe('refreshBackgroundProcesses dead-session guard', () => {
     await refreshBackgroundProcesses(SID)
 
     expect(request).toHaveBeenCalledTimes(2)
+  })
+
+  it('dismisses a stale process row when Stop is clicked after the runtime is gone', async () => {
+    reconcileBackgroundProcesses(SID, [running('stale')])
+    markSessionGone(SID)
+    $gateway.set({ request: vi.fn() } as never)
+
+    await stopBackgroundProcess(SID, 'stale')
+
+    expect(items()).toEqual([])
+  })
+
+  it('dismisses a stale process row while the gateway is disconnected', async () => {
+    reconcileBackgroundProcesses(SID, [running('disconnected')])
+    markSessionGone(SID)
+    $gateway.set(null as never)
+
+    await stopBackgroundProcess(SID, 'disconnected')
+
+    expect(items()).toEqual([])
+  })
+
+  it('dismisses and latches when Stop discovers the runtime is gone', async () => {
+    const request = vi.fn(async () => {
+      throw new Error('session not found')
+    })
+
+    reconcileBackgroundProcesses(SID, [running('rejected')])
+    $gateway.set({ request } as never)
+
+    await stopBackgroundProcess(SID, 'rejected')
+    await stopBackgroundProcess(SID, 'rejected')
+
+    expect(items()).toEqual([])
+    expect(request).toHaveBeenCalledTimes(1)
   })
 })
 

apps/desktop/src/store/composer-status.ts
@@ -8,9 +8,15 @@ import { $gateway } from './gateway'
 import { $goalsBySession, type GoalStatus } from './goals'
 import { dispatchNativeNotification } from './native-notifications'
 import { notifyError } from './notifications'
-import { isSessionGone, isSessionGoneForBackgroundPolling, markSessionGone, noteRuntimeAlive } from './runtime-gone'
+import {
+  isSessionGone,
+  isSessionGoneForBackgroundPolling,
+  markSessionGone,
+  noteRuntimeAlive,
+  resetBackgroundPollingGuard
+} from './runtime-gone'
 import { $sessions, lineageAliases } from './session'
-import { $sessionStates } from './session-states'
+import { $sessionStates, requestForOwnedSession } from './session-states'
 import { $subagentsBySession, type SubagentProgress } from './subagents'
 import { $todosBySession } from './todos'
 
@@ -401,7 +407,14 @@ export async function refreshBackgroundProcesses(sid: string): Promise<void> {
   }
 
   try {
-    const result = await gateway.request<{ processes?: GatewayProcessEntry[] }>('process.list', { session_id: sid })
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request<T>(method, params ?? {})
+    const result = await requestForOwnedSession<{ processes?: GatewayProcessEntry[] }>(
+      sid,
+      ambientRequest,
+      'process.list',
+      { session_id: sid }
+    )
 
     reconcileBackgroundProcesses(sid, result?.processes ?? [])
     // The binding answered, so it is healthy: refund the stored session's
@@ -441,10 +454,34 @@ export function dismissBackgroundProcess(sid: string, id: string) {
  *  row while the process lived on, stranding rogue tasks. On failure the row
  *  stays so the user can retry / see it didn't die. */
 export async function stopBackgroundProcess(sid: string, id: string): Promise<void> {
+  const gateway = $gateway.get()
+
+  if (isSessionGone(sid)) {
+    // The backend has already declared this runtime gone, so there is no
+    // authoritative process left to kill through this session. Remove the
+    // stale local row instead of leaving the Stop button permanently inert.
+    dismissBackgroundProcess(sid, id)
+
+    return
+  }
+
+  if (!gateway) {
+    return
+  }
+
   try {
-    await $gateway.get()?.request('process.kill', { process_id: id, session_id: sid })
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request<T>(method, params ?? {})
+    await requestForOwnedSession(sid, ambientRequest, 'process.kill', { process_id: id, session_id: sid })
     dismissBackgroundProcess(sid, id)
   } catch (err) {
+    if (isSessionGoneForBackgroundPolling(err)) {
+      dismissBackgroundProcess(sid, id)
+      markSessionGone(sid)
+
+      return
+    }
+
     notifyError(err, 'Could not stop the process')
   }
 }
@@ -471,7 +508,18 @@ export function resetSessionBackground(sid: string) {
     dismissed.add(item.id)
 
     if (item.state === 'running') {
-      void gateway?.request('process.kill', { process_id: item.id, session_id: sid }).catch(() => undefined)
+      if (gateway && !isSessionGone(sid)) {
+        const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+          gateway.request<T>(method, params ?? {})
+        void requestForOwnedSession(sid, ambientRequest, 'process.kill', {
+          process_id: item.id,
+          session_id: sid
+        }).catch(error => {
+          if (isSessionGoneForBackgroundPolling(error)) {
+            markSessionGone(sid)
+          }
+        })
+      }
     }
   }
 

apps/desktop/src/store/prompts.ts
@@ -3,6 +3,7 @@ import { atom, computed, type ReadableAtom } from 'nanostores'
 import { $clarifyRequest, $clarifyRequests } from './clarify'
 import { isSessionGone, isSessionGoneForBackgroundPolling, markSessionGone } from './runtime-gone'
 import { $activeSessionId } from './session'
+import { requestForOwnedSession } from './session-states'
 
 // Blocking interactive prompts the gateway raises mid-turn. Each maps to a
 // `*.request` event the Python side emits while it blocks the agent thread
@@ -120,10 +121,21 @@ export async function receiveApprovalRequest(gateway: ApprovalGateway | null, re
   setApprovalRequest(request)
 
   if (gateway && request.requestId && request.sessionId) {
-    await gateway.request('approval.received', {
-      request_id: request.requestId,
-      session_id: request.sessionId
-    })
+    try {
+      const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+        gateway.request(method, params ?? {}) as Promise<T>
+
+      await requestForOwnedSession(
+        request.sessionId,
+        ambientRequest,
+        'approval.received',
+        { request_id: request.requestId, session_id: request.sessionId }
+      )
+    } catch (error) {
+      if (isSessionGoneForBackgroundPolling(error)) {
+        markSessionGone(request.sessionId)
+      }
+    }
   }
 }
 
@@ -135,17 +147,21 @@ export async function replayPendingApproval(gateway: ApprovalGateway | null, ses
   let rawResult: unknown
 
   try {
-    rawResult = await gateway.request('approval.pending', {
-      session_id: sessionId
-    })
+    const ambientRequest = <T>(method: string, params?: Record<string, unknown>) =>
+      gateway.request(method, params ?? {}) as Promise<T>
+
+    rawResult = await requestForOwnedSession(
+      sessionId,
+      ambientRequest,
+      'approval.pending',
+      { session_id: sessionId }
+    )
   } catch (error) {
     if (isSessionGoneForBackgroundPolling(error)) {
       markSessionGone(sessionId)
-
-      return
     }
 
-    throw error
+    return
   }
 
   const result =